73385af7ea101a1e74a9c79839dbeaae697b9551
Megan Matsumoto <megan.matsumoto@gmail.com>
2017-06-04 08:36:03 -0700

    Quit button, color count, game over message

--- public/js/main.js
+++ public/js/main.js
@@ -278,9 +278,12 @@ $(function() {
 	console.log('*** Client Log Message: \'join_room\' payload: '+JSON.stringify(payload));
 	socket.emit('join_room',payload);
 
+	$('#quit').append('<a href="lobby.html?username='+username+'" class="btn btn-danger btn-default active" role="button" aria-pressed="true">Quit</a>');
+
+
 });
 
-
+/* Code for the board specifically */
 var old_board = [
   ['?','?','?','?','?','?','?','?'],
   ['?','?','?','?','?','?','?','?'],
@@ -331,9 +334,19 @@ socket.on('game_update',function(payload) {
 
 
     /* Animate changes to the board */
+
+    var blacksum = 0;
+    var whitesum = 0;
+
     var row,column;
     for(row = 0; row <8; row++) {
     	for(column = 0; column <8; column++) {
+    		if(board[row][column] == 'b') {
+    			blacksum++;
+    		}
+    		if(board[row][column] == 'w') {
+    			whitesum++;
+    		}
     		/* If a board space has changed */
             if(old_board[row][column] != board [row][column]) {
             	if(old_board[row][column] == '?' && board[row][column] == ' ') {
@@ -391,6 +404,8 @@ socket.on('game_update',function(payload) {
     	}
     }
 
+    $('#blacksum').html(blacksum);
+    $('#whitesum').html(whitesum);
     old_board = board;
 
 });
@@ -403,6 +418,21 @@ socket.on('play_token_response',function(payload) {
         alert(payload.message);
     	return;
     }
+});
+
+
+socket.on('game_over',function(payload) {
+	console.log('*** Client Log Message: \'game_over\'\n\tpayload: '+JSON.stringify(payload));
+    /*Check for a good play_token_response update */
+    if(payload.result == 'fail') {
+        console.log(payload.message);
+    	return;
+    }
+
+    /* Jump to a new page */
+    $('#game_over').html('<h1>Game Over</h1><h2>'+payload.who_won+' won!</h2>');
+    $('#game_over').append('<a href="lobby.html?username='+username+'" class="btn btn-success btn-lg active" role="button" aria-pressed="true">Return to the lobby</a>');
+
 });
 
 
